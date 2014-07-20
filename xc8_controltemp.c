3a3568f2524b7f72e34a90e3795086a5b6b4f5f5
ELECTRONICSOB <santiago_sob@hotmail.com>
2014-07-20 16:22:11 -0700

    control
    control de temperatura y velocidad de un motor

---
+++ xc8_controltemp.c
@@ -0,0 +1,89 @@
+/* 
+ * File:   xc8_controltemp.c
+ * Author: SANTIAGO
+ *
+ * Created on 15 de julio de 2014, 09:50 PM
+ */
+
+#include <xc.h>
+#include "fuses.h"
+#include "types.h"
+#include "system/system.h"
+#include "hd44780/hd44780.h"
+#include "printf/printf.h"
+#include "pwm/pwm.h"
+#include "adc/adc.h"
+#include "gpios/gpios.h"
+#include "delays/delays.h"
+
+int main(void)
+{
+
+    _U16 adc,adc2,adc3;
+     _U08 Duty = 0;
+      _U16 setpot;
+      _U16 tem;
+       _U16 pot;
+
+
+    ANCON0 = 0XFF;  /*Desactivamos las entradas analogicas*/
+    ANCON1 = 0XFF;  /*Desactivamos las entradas analogicas*/
+
+     Adc_Init(ADC_8BITS); /*se inciliza el adc con resolucion de 8bits*/
+
+   System_PeripheralPinSelect(CompareOrPWMOutputChannel1A, 2);   /*PWM1_A en el pin RP2 (A5)*/
+    Gpios_PinDirection(GPIOS_PORTA, 5, GPIOS_OUTPUT);  /*pin RA5 como salida para PWM_1A*/
+    
+     Gpios_PinDirection(GPIOS_PORTB,6,GPIOS_OUTPUT);/*control giro (B6) salida*/
+      Gpios_PinDirection(GPIOS_PORTB,5,GPIOS_OUTPUT);/*control giro (B5) salida*/
+ Gpios_PinDirection(GPIOS_PORTB,2,GPIOS_OUTPUT);/*control giro (B5) salida*/
+ 
+      Pwm_Init(PWM_PORT1, 30000); /*señal a 20KHz*/
+
+      HD44780_Init();                                     /*inicliza el LCD*/
+      xdev_out(HD44780_WriteData); /*establece elcd como salida*/
+
+    while (1)
+    {
+   adc= Adc_u16Read(3); /*Se lee la lectura del canal 3 en el adc  A3*/
+   adc2= Adc_u16Read(2);
+   adc3= Adc_u16Read(7); //e2
+
+     tem = (adc/2.2);
+     setpot=(adc2/2.2);
+     pot = (adc3/2.2);
+    
+                 
+            HD44780_SetCursor(0,1);               /*se cambia el cursor a la linea 2 columna 5*/
+            xprintf("set%3d",setpot);
+            Delays_ms(50);
+
+            HD44780_SetCursor(1,1);               /*se cambia el cursor a la linea 2 columna 5*/
+            xprintf("tem%3d",tem); //  en esta parte se utilizara un potenciometro  en ves del sensor
+            Delays_ms(50);
+
+             HD44780_SetCursor(0,8);
+              xprintf("vel%3d",pot); //  en esta parte se utilizara un potenciometro  en ves del sensor
+               Delays_ms(50);                  /*se cambia el cursor a la linea 2 columna 5*/
+                    
+                                 /*apago pin RA2*/
+            
+               
+            if(tem<setpot)
+               {
+                   Gpios_WritePin(GPIOS_PORTB, 2, 0);
+
+                   Pwm_DutyCycle(PWM_PORT1, PWM_CHANNEL_A, 120); /*Señal al 50%*/
+               }   
+               
+            if(tem>setpot)
+            {
+               Gpios_WritePin(GPIOS_PORTB, 2, 1);  /*enciendo pin RA2*/
+
+               Gpios_WritePin(GPIOS_PORTB,6,1); /*giro en un sentido*/
+                  Gpios_WritePin(GPIOS_PORTB,5,0);
+                  Pwm_DutyCycle(PWM_PORT1, PWM_CHANNEL_A, pot); /*Señal al 50%*/
+               }
+                
+           }
+    }
